a2c4591f1bedb79463f0e9558833758e5849bacb
JestinJoseph476 <vala0025@algonquinlive.com>
2020-01-12 22:02:48 -0500

    added mathematical functions to the file.

---
+++ javscript.js
@@ -0,0 +1,12 @@
+var name='Jestin Joseph'
+console.log(`Hello, My name is ${name}.`)
+const number=6
+console.log(`selected integer = ${number}`);
+const multi = number*2
+console.log(`${number} X 2 = ${multi}`);
+const product = multi*5
+console.log(`${multi} X 5 = ${product}`);
+const division = product/number
+console.log(`${product} / ${number} = ${division}`);
+const substract = division - 7
+console.log(`${division} - 7 = ${substract}`);
